1d6b0640d16bc2982d2edadae72bccefa6b8cefb
lfborjas <luis.borjas@escolarea.com>
2011-03-08 01:10:54 -0600

    casi lo de frecuencias

--- parcial3/archivos/simple_streams/Frecuencias.java
+++ parcial3/archivos/simple_streams/Frecuencias.java
@@ -3,16 +3,16 @@ import java.util.TreeMap;
 import java.util.Map;
 
 public class Frecuencias{
-    public static void plot(String origin) throws Exception{
+    public static void plotChars(String origin) throws Exception{
         TreeMap<Character, Integer> tabla = new TreeMap<Character, Integer>();
            
         //abrir el archivo, podría no encontrarlo
-        FileInputStream in = new FileInputStream(origin);
+        FileReader in = new FileReader(origin);
         int c = 0;
         
         //leer hasta consumir el flujo
         while( (c = in.read() ) != -1){
-            if((c >= 65 && c <= 90 ) || (c >= 97 && c <= 122)){
+            if((c >= 'A' && c <= 'Z' ) || (c >= 'a' && c <= 'z')){
                 char leChar = Character.toLowerCase((char)c);
                 if(tabla.get(leChar) != null){
                     tabla.put(leChar, tabla.get(leChar)+1);
@@ -23,24 +23,51 @@ public class Frecuencias{
         }
         //siempre cerrar ! Si no, el flujo se corrompe
         in.close();
+        printHistogram(tabla);
+
+    }
+    
+    private static void printHistogram(TreeMap<?, Integer> histogram){
         System.out.println("Histograma de frecuencias:\n-------------------------");
-        for(Map.Entry<Character, Integer> entry : tabla.entrySet()){
+        for(Map.Entry<?, Integer> entry : histogram.entrySet()){
             System.out.print(entry.getKey()+": ");
-            for(int i=0; i< entry.getValue(); i++)
+            for(int i=0; i< (Integer)entry.getValue(); i++)
                 System.out.print("*");
             System.out.println("");
         }
+    }
 
+    public static void plotWords(String origin) throws Exception{
+        TreeMap<String, Integer> tabla = new TreeMap<String, Integer>();
+        
+        BufferedReader in = new BufferedReader(new FileReader(origin));
+        String line = "";
+        //leer hasta consumir el flujo
+        while( (line = in.readLine() ) != null){
+            for(String word : line.split("\\W+")){
+                if(tabla.get(word) != null){
+                    tabla.put(word, tabla.get(word)+1);
+                }else{
+                    tabla.put(word, 1);
+                }
+            }
+        }
+        //siempre cerrar ! Si no, el flujo se corrompe
+        in.close();
+        printHistogram(tabla);
     }
 
     public static void main (String [] args) throws Exception
     {
         switch(args.length){
             case 1:
-                plot(args[0]);
+                plotChars(args[0]);
+                break;
+            case 2:
+                plotWords(args[1]);
                 break;
             default:
-                System.out.println("Uso: java Frecuencias [contenido] archivo");
+                System.out.println("Uso: java Frecuencias [--palabras] archivo");
                 break;
         }
     }
